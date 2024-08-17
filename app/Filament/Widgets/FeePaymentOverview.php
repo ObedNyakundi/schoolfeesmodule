7a5ffeb72fbc692591fa36738530c9a3e06c9f27
Obed Nyakundi <real.obedpaul@gmail.com>
2024-08-17 20:44:47 +0300

    a few admin panel widgets

---
+++ app/Filament/Widgets/FeePaymentOverview.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Carbon\Carbon;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use App\Models\Student;
+use App\Models\FeePayment;
+use App\Models\StudentAccount;
+
+class FeePaymentOverview extends BaseWidget
+{
+    protected static ?int $sort = 2;
+
+    protected function getStats(): array
+    {
+        return [
+            //all payments
+            Stat::make('Total Fee Paid', FeePayment::sum('amount'))
+            -> description('All Fee Collected')
+            -> descriptionIcon('heroicon-o-arrow-right')
+            ->icon('heroicon-o-banknotes')
+            ->color('success')
+            ->chart([
+                '4','5','3','7','9','6','8'
+            ]),
+
+            //payments made this month
+            Stat::make('This Month\'s Collection', 
+                FeePayment::whereMonth('created_at', Carbon::now()->month)
+                                    ->sum('amount'))
+            -> description('Fee Collected this month')
+            -> descriptionIcon('heroicon-o-arrow-right')
+            ->icon('heroicon-o-banknotes')
+            ->color('success')
+            ->chart([
+                '4','5','3','7','9','6','8'
+            ]),
+
+            //Fee Balances
+            Stat::make('School Account Balance', 
+                StudentAccount::sum('balance'))
+            -> description('Account Balance')
+            -> descriptionIcon('heroicon-o-arrow-right')
+            ->icon('heroicon-o-banknotes')
+            ->color('warning')
+            ->chart([
+                '4','5','3','7','9','6','8'
+            ]),
+        ];
+    }
+
+    //private function
+}
---
+++ app/Filament/Widgets/StudentsOverview.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Carbon\Carbon;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use App\Models\Student;
+
+class StudentsOverview extends BaseWidget
+{
+    protected static ?int $sort = 1;
+
+    protected function getStats(): array
+    {
+        return [
+
+            //Students Widgets
+            Stat::make('Total Students', Student::count())
+            -> description('All Admitted Students')
+            -> descriptionIcon('heroicon-o-arrow-trending-up')
+            ->icon('heroicon-o-users')
+            ->color('success')
+            ->chart([
+                '4','5','3','7','9','6','8'
+            ]),
+
+            Stat::make('Recently Admitted', 
+                    Student::whereMonth('created_at', Carbon::now()->month)
+                   ->get()
+                   ->count())
+            -> description('Admitted This Month')
+            -> descriptionIcon('heroicon-o-arrow-right')
+            ->icon('heroicon-o-clock')
+            ->color('success')
+            ->chart([
+                '4','6','5','7','4','6','6'
+            ]),
+
+            Stat::make('Admissions per Class', Student::whereMonth('created_at', Carbon::now()->month)
+                   ->distinct('stream_id')
+                   ->count())
+            -> description('Classes that Admitted This Month')
+            -> descriptionIcon('heroicon-o-arrow-right')
+            ->icon('heroicon-o-building-library')
+            ->color('primary')
+            ->chart([
+                '5','5','7','7','5','5','4'
+            ]),
+
+        ];
+    }
+}
