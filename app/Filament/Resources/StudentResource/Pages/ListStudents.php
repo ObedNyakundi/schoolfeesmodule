9bc9402ed9885d5e45ec9600d4fd96435b1a60db
Obed Nyakundi <real.obedpaul@gmail.com>
2024-08-18 19:54:13 +0300

    added tabs

--- app/Filament/Resources/StudentResource/Pages/ListStudents.php
+++ app/Filament/Resources/StudentResource/Pages/ListStudents.php
@@ -5,6 +5,10 @@ namespace App\Filament\Resources\StudentResource\Pages;
 use App\Filament\Resources\StudentResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Pages\ListRecords\Tab;
+use Illuminate\Database\Eloquent\Builder;
+
+use App\Models\Student;
 
 class ListStudents extends ListRecords
 {
@@ -15,5 +19,23 @@ class ListStudents extends ListRecords
         return [
             Actions\CreateAction::make(),
         ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'All' => Tab::make()
+            ->badge(Student::count()),
+
+            'With Fee Balance' => Tab::make()
+            ->modifyQueryUsing(fn (Builder $query): Builder => $query->whereHas('studentAccount', fn (Builder $query) => $query->where('balance', '<', 0)))
+            ->badge(Student::query()->whereHas('studentAccount', fn (Builder $query) => $query->where('balance', '<', 0))->count()),
+
+            'Without Fee Balance' => Tab::make()
+            ->modifyQueryUsing(fn (Builder $query): Builder => $query->whereHas('studentAccount', fn (Builder $query) => $query->where('balance', '>', -1)))
+            ->badge(Student::query()->whereHas('studentAccount', fn (Builder $query) => $query->where('balance', '>', -1))->count()),
+
+            ];
+        
     }
 }
